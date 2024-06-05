127e6dd96fde0fbca1d96839a4413eace2ae77af
lara <lara@mail.com>
2024-06-05 11:59:56 -0300

    Co-authored-by: VicenteRuBa <VicenteRuBa@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Formulario from '././views/Formulario';
+import Listado from '././views/Listado';
 
 function App() {
+  const [citas, setCitas] = useState([]);
+
+  const agregarCita = (nuevaCita) => {
+    setCitas([...citas, nuevaCita]);
+  };
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    setCitas(nuevasCitas);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formulario agregarCita={agregarCita} />
+      <Listado citas={citas} eliminarCita={eliminarCita} />
     </div>
   );
 }
