7581a08e985ee0e550c579e63e6feadbaedcf31f
lara <lara@mail.com>
2024-06-10 13:37:26 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+// App.jsx
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Formulario from '././views/Formulario';
-import Listado from '././views/Listado';
+import Formulario from './views/Formulario';
+import Listado from './views/Listado';
 
 function App() {
-  const [citas, setCitas] = useState([]);
+  // Obtener las citas almacenadas del localStorage al cargar la página
+  const citasGuardadas = JSON.parse(localStorage.getItem('citas'));
+  const [citas, setCitas] = useState(citasGuardadas || []);
+
+  // Guardar las citas en el localStorage cada vez que cambien
+  useEffect(() => {
+    localStorage.setItem('citas', JSON.stringify(citas));
+  }, [citas]);
 
   const agregarCita = (nuevaCita) => {
     setCitas([...citas, nuevaCita]);
